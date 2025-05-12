018014f2c66999e484d1ae791f6b9258b28b1198
lamarquenet <nicolaszuain3@hotmail.com>
2025-05-11 21:52:44 -0300

    agregando smythery fix y cambios a stremeable

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -5,11 +5,9 @@ export const auth = (req: Request, res: Response, next: NextFunction): any => {
   const urlObject = new URL(url, `${req.protocol}://${req.headers.host}`);
   const searchParams = Object.fromEntries(urlObject.searchParams.entries());
   const token = req.query['token'] ?? req.headers['token'] ?? searchParams['token'] ?? '';
-  const chatGptApiKey = req.query['chatGptApiKey'] ?? req.headers['chatGptApiKey'] ?? searchParams['chatGptApiKey'] ?? '';
 
   (req as any).auth = {
-    token,
-    chatGptApiKey
+    token
   };
 
   /*if (!token) {
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -4,5 +4,4 @@ export interface Context {
   
   export interface AuthInfo {
     token: string ;
-    chatGptApiKey: string;
   }
